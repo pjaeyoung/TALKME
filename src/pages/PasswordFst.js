dd77c88abda79cf0cfee36806c9a6ed1740092dd
정인호 <jeong-inho@eNoLJ-MacBookAir.local>
2020-08-20 21:29:52 +0900

    :hammer: fix: bug 수정

--- src/pages/PasswordFst.js
+++ src/pages/PasswordFst.js
@@ -8,6 +8,7 @@ class PasswordFst extends React.Component {
     super(props);
     this.state = {
       email: "",
+      loading: false
     };
   }
   // 뒤로가기 버튼 클릭 시 login page로 이동
@@ -15,15 +16,17 @@ class PasswordFst extends React.Component {
     this.props.handlingIsLogin("findPassword");
     this.props.history.push("/login");
   }
-  // state의 email을 입력받은 email로 변경
-  changeEmail(email) {
+  // state의 key와 value를 입력 받아 변경
+  changeState(key, value) {
     this.setState({
-      email: email,
-    });
+      [key]: value
+    })
   }
   // send 버튼 클릭 시 secret key 발급 API 요청 후 passwordsnd page로 이동
   sendEmail() {
-    fetch("http://ec2-13-124-126-40.ap-northeast-2.compute.amazonaws.com:4000/auth/pwinquiry/askkey", {
+    this.changeState("loading", true);
+
+    fetch("/auth/pwinquiry/askkey", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -40,6 +43,7 @@ class PasswordFst extends React.Component {
           );
           this.props.history.push("/passwordsnd");
         } else {
+          this.changeState("loading", false);
           document.querySelector("#mobile").classList.add("wobble-horizontal")
           window.setTimeout(() => {
             document.querySelector("#mobile").classList.remove("wobble-horizontal")
@@ -52,49 +56,60 @@ class PasswordFst extends React.Component {
   render() {
     return (
       <>
-        <button className="passwordBack" onClick={() => this.backBtn()}>
-          <i className="fas fa-arrow-left"></i>
-        </button>
-        <div className="PWshootingStarAnimationBox">
-          <div className="shootingStarAnimation">
-            <i id="star" className="fas fa-star fa-lg">
-              <div id="bigStarBack"></div>
-            </i>
-            <div className="allSatellite s1"></div>
-            <div className="allSatellite s2"></div>
-            <div className="allSatellite s3"></div>
-            <div className="allSatellite s4"></div>
-            <div className="allSatellite s5"></div>
-            <div className="meteor"></div>
-            <div className="meteor s6"></div>
-            <div className="meteor s7"></div>
-            <div className="meteor s8"></div>
-          </div>
-        </div>
-        <div id="PWText">Enter your email for new password</div>
-        <div className="PWInputBox">
-          <input
-            onFocus={(e) => (e.target.placeholder = "")}
-            onBlur={(e) => (e.target.placeholder = "Please Enter Your Email")}
-            placeholder="Please Enter Your Email"
-            id="EmailBOX"
-            type="email"
-            onChange={(e) => this.changeEmail(e.target.value)}
-            onKeyPress={(e) => {
-              if (e.key === "Enter") {
-                this.sendEmail();
-                e.target.value = "";
-              }
-            }}
-          ></input>
-          <img className="mail" src={mail}></img>
-        </div>
+        {!this.state.loading ?
+          <>
+            <button className="passwordBack" onClick={() => this.backBtn()}>
+              <i className="fas fa-arrow-left"></i>
+            </button>
+            <div className="PWshootingStarAnimationBox">
+              <div className="shootingStarAnimation">
+                <i id="star" className="fas fa-star fa-lg">
+                  <div id="bigStarBack"></div>
+                </i>
+                <div className="allSatellite s1"></div>
+                <div className="allSatellite s2"></div>
+                <div className="allSatellite s3"></div>
+                <div className="allSatellite s4"></div>
+                <div className="allSatellite s5"></div>
+                <div className="meteor"></div>
+                <div className="meteor s6"></div>
+                <div className="meteor s7"></div>
+                <div className="meteor s8"></div>
+              </div>
+            </div>
+            <div id="PWText">Enter your email for new password</div>
+            <div className="PWInputBox">
+              <input
+                onFocus={(e) => (e.target.placeholder = "")}
+                onBlur={(e) => (e.target.placeholder = "Please Enter Your Email")}
+                placeholder="Please Enter Your Email"
+                id="EmailBOX"
+                type="email"
+                onChange={(e) => this.changeState("email", e.target.value)}
+                onKeyPress={(e) => {
+                  if (e.key === "Enter") {
+                    this.sendEmail();
+                    e.target.value = "";
+                  }
+                }}
+              ></input>
+              <img className="mail" src={mail}></img>
+            </div>
 
-        <div id="btnBox">
-          <div id="PWBtn" onClick={() => this.sendEmail()}>
-            <div id="PWBtnText">Send</div>
-          </div>
-        </div>
+            <div id="btnBox">
+              <div id="PWBtn" onClick={() => this.sendEmail()}>
+                <div id="PWBtnText">Send</div>
+              </div>
+            </div>
+          </>
+          :
+          <div className="loadingAnimationBox">
+            <div id="loadingStick1" className="loadingStick"></div>
+            <div id="loadingStick2" className="loadingStick"></div>
+            <div id="loadingStick3" className="loadingStick"></div>
+            <div id="loadingStick4" className="loadingStick"></div>
+            <div id="loadingStick5" className="loadingStick"></div>
+          </div>}
       </>
     );
   }
