38df87f869465f8b6f89e2ecde9d9488df0b6c45
shinuhyun <shinuhyun@gmail.com>
2020-08-20 10:50:21 +0900

    :sparkles: feat : 비밀번호 1,2,3 CSS 작성 비밀번호 1,2,3 CSS 작성했습니다.

--- src/pages/PasswordSnd.js
+++ src/pages/PasswordSnd.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import "../css/PasswordSnd.css";
 
 class PasswordSnd extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      secretKey: ""
+      secretKey: "",
     };
   }
   // 뒤로가기 버튼 클릭 시 passwordfst page로 이동
@@ -15,7 +16,7 @@ class PasswordSnd extends React.Component {
   // state의 secretkey를 입력받은 secretkey로 변경
   changeSecretKey(key) {
     this.setState({
-      secretKey: key
+      secretKey: key,
     });
   }
   // submit 버튼 클릭 시 입력한 secret key가 일치하는지 확인하는 API 요청 후 passwordtrd page로 이동
@@ -23,32 +24,66 @@ class PasswordSnd extends React.Component {
     fetch("/auth/pwinquiry/comparekey", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        secretKey: this.state.secretKey
+        secretKey: this.state.secretKey,
       }),
-      credentials: "include"
+      credentials: "include",
     })
-      .then(res => {
+      .then((res) => {
         if (res.ok) {
-          this.props.history.push('/passwordtrd');
+          this.props.history.push("/passwordtrd");
         } else {
           alert("Secret Key가 일치하지 않습니다.");
         }
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   }
 
   render() {
     return (
-      <div>
-        <button onClick={() => this.backBtn()}>뒤로가기</button>
-        <div>별똥별 애니메이션</div>
-        <div>Enter the secret key written in the email received</div>
-        <input type="text" onChange={e => this.changeSecretKey(e.target.value)}></input>
-        <button onClick={() => this.submitSecretKey()}>submit</button>
-      </div>
+      <>
+        <button id="passwordBack" onClick={() => this.backBtn()}>
+          <i className="fas fa-arrow-left"></i>
+        </button>
+        <div className="shootingStarAnimationBox">
+          <div className="shootingStarAnimation">
+            <div className="star"></div>
+
+            <div id="PWSndText">
+              Enter the secret key
+              <br />
+              written in the email you recieved.
+            </div>
+          </div>
+        </div>
+
+        <div className="PWSndInputBox">
+          <input
+            onFocus={(e) => (e.target.placeholder = "")}
+            onBlur={(e) =>
+              (e.target.placeholder = "Please Enter Your Secret Key")
+            }
+            placeholder="Please Enter Your Secret Key"
+            id="SecretKeyBOX"
+            type="text"
+            onChange={(e) => this.changeSecretKey(e.target.value)}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                this.handlingUserInformation();
+                e.target.value = "";
+              }
+            }}
+          ></input>
+        </div>
+
+        <div id="btnBox">
+          <div id="PWSndBtn" onClick={() => this.submitSecretKey()}>
+            <div id="PWSndSubmit">Submit</div>
+          </div>
+        </div>
+      </>
     );
   }
 }
